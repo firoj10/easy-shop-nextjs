1d69485dba0bca1666d6877569c61316fc329b43
firoj10 <mdfirojhasan.info@gmail.com>
2023-08-21 01:14:52 +0600

    product added:

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,7 +3,11 @@ export const metadata = {
 };
 
 const AboutPage = () => {
-  return <div>AboutPage</div>;
+  return <div>AboutPage
+<h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni et mollitia cupiditate, aspernatur rerum quasi ullam optio quis necessitatibus culpa.</h2>
+
+
+  </div>;
 };
 
 export default AboutPage;
